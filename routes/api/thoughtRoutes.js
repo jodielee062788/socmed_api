e6207ae35764a1de921150063285ae0f55b85f20
Jodie <jodielee062788@gmail.com>
2024-03-29 19:25:02 +1100

    Create thought controller

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought
+} = require('../../controllers/courseController.js');
+
+router.route('/').get

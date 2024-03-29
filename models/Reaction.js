00faa385ce3f2d7af665b55b7fdc7febc2b9155f
Jodie <jodielee062788@gmail.com>
2024-03-29 17:03:57 +1100

    Define reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const { Schema, Types } = require('mongoose');
+
+// Define reaction schema
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        // Getter method to format timestamp
+        get: createdAt => {
+            return new Date(createdAt).toLocaleString();
+        }
+      },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+module.exports = reactionSchema;
